c6054de8f5ea0917ad793e1e3471f6351293c125
Medzo02 <111018812+Medzo02@users.noreply.github.com>
2022-10-15 23:21:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const menuicon = document.querySelector(".menuicon");
-// const croix = document.querySelector("#croix");
 const menu = document.querySelector(".menu");
-const mobile1 = document.querySelector("#mobile1");
 const mobile2 = document.querySelector("#mobile2");
 
 menuicon.addEventListener("click", () => {
@@ -14,10 +12,8 @@ croix.addEventListener("click", () => {
   menu.classList.toggle("menuback");
 });
 setInterval(() => {
-  mobile2.classList.toggle("animmon");
   mobile2.style.opacity = "0";
 }, 5000);
 setInterval(() => {
-  mobile2.classList.toggle("animmon");
   mobile2.style.opacity = "1";
 }, 10000);
